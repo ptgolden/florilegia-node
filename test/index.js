6b6a865d839cd0833b63c1f565bd4073b55e5192
Patrick Golden <ptgolden0@gmail.com>
2016-11-06 18:03:07 -0500

    Refactor tests to easily test different applications

--- test/index.js
+++ test/index.js
@@ -2,22 +2,52 @@
 
 const fs = require('fs')
     , path = require('path')
-    , test = require('tape')
+    , test = require('blue-tape')
     , { getAnnotations } = require('../build/Release/pdf2oac')
 
 
+function getPdfCreator(filename) {
+  return filename.replace(/.*\//, '').replace('.pdf', '');
+}
+
+function testFiles(t, files, expected, msg) {
+}
+
+const cases = [
+  {
+    dir: './pdfs/0_none',
+    expected: [],
+    msg: 'should not find any annotations on an empty document'
+  },
+
+  {
+    dir: './pdfs/1_comment',
+    expected: [
+      {
+          page: 1,
+          motivation: 'commenting',
+          body_type: 'text',
+          body_text: 'This is a comment',
+          body_label: null
+      }
+    ],
+    msg: 'should extract comment annotations'
+  }
+]
+
 test('Extracting annotations', t => {
-  t.plan(2);
+  const tests = []
 
-  const a = getAnnotations(path.join(__dirname, './none.pdf'));
-  t.deepEqual(a, [], 'should find no annotations on a document without them.');
+  cases.forEach(({ dir, expected, msg }) => {
+    const pdfs = fs.readdirSync(path.join(__dirname, dir))
 
-  const b = getAnnotations(path.join(__dirname, './comment.pdf'));
-  t.deepEqual(b, [{
-    page: 1,
-    motivation: 'commenting',
-    body_type: 'text',
-    body_text: 'This is a comment',
-    body_label: null
-  }]);
+    pdfs.forEach(filename => {
+      t.deepEqual(
+        getAnnotations(path.join(__dirname, dir, filename)),
+        expected,
+        `${msg} (${getPdfCreator(filename)}).`)
+    })
+  })
+
+  return Promise.resolve();
 })
