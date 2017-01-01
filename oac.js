42ad88e7fe71e8948fb582793d34a2b732c1bc67
Patrick Golden <ptgolden0@gmail.com>
2017-01-01 16:54:45 -0500

    Refactor using rdf-triple-builder library

--- oac.js
+++ oac.js
@@ -6,7 +6,7 @@ const fs = require('fs')
     , { Writer, Util } = require('n3')
     , { getAnnotations } = require('./')
 
-const PREFIXES = {
+const prefixes = {
   oa: "http://www.w3.org/ns/oa#",
   dc: "http://purl.org/dc/elements/1.1/",
   dctypes: "http://purl.org/dc/dcmitype/",
@@ -15,6 +15,8 @@ const PREFIXES = {
   rdf: "http://www.w3.org/1999/02/22-rdf-syntax-ns#",
   rdfs: "http://www.w3.org/2000/01/rdf-schema#",
 }
+
+const $ = require('rdf-triple-builder')({ prefixes })
 
 function makeTag(email, filename) {
   const d = new Date()
@@ -27,112 +29,82 @@ function makeTag(email, filename) {
   ].join(':')
 }
 
-function format(val) {
-  val = val.toString();
-
-  if (Util.isLiteral(val)) {
-    return val;
-  }
-
-  if (Util.isPrefixedName) {
-    return Util.expandPrefixedName(val, PREFIXES)
-  }
-}
-
-function tripleFactory(subject) {
-  const func = predicate => object => ({
-    subject: format(subject),
-    predicate: format(predicate),
-    object: format(object),
-  })
-
-  func.toString = () => subject;
-
-  return func;
-}
-
 function pdf2oac(user='http://example.com', filename, cb) {
-  const writer = new Writer({ prefixes: PREFIXES, format: 'ntriples' })
+  const writer = new Writer({ prefixes, format: 'ntriples' })
       , annotations = getAnnotations(filename)
 
   let triples = []
 
   annotations.forEach((annotation, i) => {
     const { page, motivation, body_text, highlighted_text } = annotation
-        , annotURI = tripleFactory(makeTag(user, filename, `annot-${i + 1}`))
-        , makeURI = p => `${annotURI}/${p}`
-        , targetURI = tripleFactory(makeURI('target'))
-        , pdfSelectorURI = tripleFactory(makeURI('page-selector'))
+        , $annot = $(makeTag(user, filename, `annot-${i + 1}`))
+        , makeURI = p => $(`${$annot}/${p}`)
+        , $target = makeURI('target')
+        , $pdfSelector = makeURI('page-selector')
 
-    triples = triples.concat([
-      annotURI('rdf:type')('oa:Annotation'),
-      annotURI('oa:hasMotivation')('oa:' + motivation),
-      annotURI('oa:hasTarget')(targetURI),
-      annotURI('oa:hasSource')('file://' + filename),
-    ])
+    triples = triples.concat(
+      $annot({
+        'rdf:type': 'oa:Annotation',
+        'oa:hasMotivation': `oa:${motivation}`,
+        'oa:hasTarget': $target,
+        'oa:hasSource': `file://${filename}`,
+      }),
 
-    triples = triples.concat([
-      targetURI('oa:hasSelector')(pdfSelectorURI),
-      pdfSelectorURI('rdf:type')('oa:FragmentSelector'),
-      pdfSelectorURI('rdf:value')(Util.createLiteral(`#page=${page}`)),
-      pdfSelectorURI('oa:conformsTo')('http://tools.ietf.org/rfc/rfc3778'),
-    ])
+      $target('oa:hasSelector')($pdfSelector),
+
+      $pdfSelector({
+        'rdf:type': 'oa:FragmentSelector',
+        'rdf:value': Util.createLiteral(`#page=${page}`),
+        'oa:conformsTo': 'http://tools.ietf.org/rfc/rfc3778',
+      })
+    )
+
     // Text selector
     if (highlighted_text) {
-      const textSelectorURI = tripleFactory(makeURI('text-selector'))
+      const $textSelector = makeURI('text-selector')
 
-      triples = triples.concat([
-        targetURI('oa:hasSelector')(textSelectorURI),
-        textSelectorURI('rdf:type')('oa:TextQuoteSelector'),
-        textSelectorURI('oa:exact')(Util.createLiteral(highlighted_text)),
-      ])
+      triples = triples.concat(
+        $target('oa:hasSelector')($textSelector),
+
+        $textSelector({
+          'rdf:type': 'oa:TextQuoteSelector',
+          'oa:exact': Util.createLiteral(highlighted_text),
+        })
+      )
     }
 
     // Text body (i.e. comment)
     if (body_text) {
-      const commentBodyURI = tripleFactory(makeURI('comment-body'))
+      const $commentBody = makeURI('comment-body')
 
-      triples = triples.concat([
-        annotURI('oa:hasBody')(commentBodyURI),
-        commentBodyURI('dc:format')(Util.createLiteral('text/plain')),
-        commentBodyURI('rdf:type')('dctypes:Text'),
-        commentBodyURI('rdf:type')('cnt:ContentAsText'),
-        commentBodyURI('cnt:chars')(Util.createLiteral(body_text)),
-      ])
+      triples = triples.concat(
+        $annot('oa:hasBody')($commentBody),
 
-      commentBodyURI({
-        'dc:format': Util.createLiteral('text/plain'),
-        'rdf:type': [
-          'dctypes:Text',
-          'cnt:ContentAsText'
-        ],
-        'cnt:chars': Util.createLiteral(body_text)
-      })
-
-      triples = triples.concat([
-        annotURI('oa:hasBody')(commentBodyURI),
-        commentBodyURI('dc:format')(Util.createLiteral('text/plain')),
-        commentBodyURI('rdf:type')('dctypes:Text'),
-        commentBodyURI('rdf:type')('cnt:ContentAsText'),
-        commentBodyURI('cnt:chars')(Util.createLiteral(body_text)),
-      ])
+        $commentBody({
+          'dc:format': Util.createLiteral('text/plain'),
+          'rdf:type': ['dctypes:Text', 'cnt:ContentAsText'],
+          'cnt:chars': Util.createLiteral(body_text),
+        })
+      )
     }
 
     if (motivation === 'bookmarking') {
       const file = glob.sync('TEST/' + annotation.object_id + '*').slice(-1)[0];
 
       if (file) {
-        const imageBodyURI = tripleFactory(makeURI('stamp'))
+        const $imageBody = makeURI('stamp')
             , content = fs.readFileSync(file)
             , buf = new Buffer(content);
 
-        triples = triples.concat([
-          annotURI('oa:hasBody')(imageBodyURI),
-          imageBodyURI('dc:format')(Util.createLiteral('image/png')),
-          imageBodyURI('rdf:type')('dctypes:Image'),
-          imageBodyURI('rdf:type')('cnt:ContentAsBase64'),
-          imageBodyURI('cnt:bytes')(Util.createLiteral(buf.toString('base64'))),
-        ])
+        triples = triples.concat(
+          $annot('oa:hasBody')($imageBody),
+
+          $imageBody({
+            'dc:format': Util.createLiteral('image/png'),
+            'rdf:type': ['dctypes:Image', 'cnt:ContentAsBase64'],
+            'cnt:bytes': Util.createLiteral(buf.toString('base64')),
+          })
+        )
       }
 
     }
