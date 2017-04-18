9f8d3c1aec240c2fd10410f730ced6ec5c97750b
Patrick Golden <ptgolden0@gmail.com>
2017-04-18 13:55:07 -0400

    Try out glib

--- binding.gyp
+++ binding.gyp
@@ -4,22 +4,20 @@
 			"target_name": "pdf2oac",
 			"sources": [
 				"cpp/pdf2oac.cc",
-				"lib/ImageOutputDev.cc"
 			],
 			"libraries": [
-				"<!@(pkg-config --libs poppler)",
+				"<!@(pkg-config --libs poppler-glib)",
 			],
 			"include_dirs": [
 				"<!(node -e \"require('nan')\")",
 				"<!(node -e \"require('streaming-worker-sdk')\")",
-				"<!@(pkg-config poppler --cflags-only-I | sed s/-I//g)",
-				"lib",
-
+				"<!@(pkg-config poppler-glib --cflags-only-I | sed s/-I//g)",
+				"lib"
 			],
 			"cflags": [
 				"-Wall",
 				"-std=c++11",
-				"<!@(pkg-config --cflags poppler)"
+				"<!@(pkg-config --cflags poppler-glib)"
 			]
 		}
   ]
--- cpp/pdf2oac.cc
+++ cpp/pdf2oac.cc
@@ -4,168 +4,79 @@
 #include <list>
 
 #include <nan.h>
-
 #include "streaming-worker.h"
 
 #define JSON_NOEXCEPTION 1
 #include "json.hpp"
-
+#include "base64.h"
 #define MULTITHREADED 1
 
-#include <goo/GooString.h>
-#include <Annot.h>
-#include <CharTypes.h>
 #include <GlobalParams.h>
-#include <ImageOutputDev.h>
-#include <Page.h>
-#include <PDFDoc.h>
-#include <PDFDocEncoding.h>
-#include <PDFDocFactory.h>
-#include <TextOutputDev.h>
-#include <UnicodeMap.h>
-#include <UTF.h>
-#include <XRef.h>
-
-using json = nlohmann::json;
+#include <poppler.h>
+#include <glib.h>
 
 struct annotation_t {
 	int page;
-	int object_id;
+	// int object_id;
 	const char* motivation;
 	const char* stamp_label;
 	std::string body_text;
 	std::string highlighted_text;
+	std::string body_png;
 };
 
-struct rect_t {
-	double xMin;
-	double yMin;
-	double xMax;
-	double yMax;
-};
-
-std::string gooStringToStdString(UnicodeMap *u_map, GooString *rawString) {
-	Unicode *u;
-	char buf[8];
-	std::string markup_text = "";
-
-	if (u_map) {
-		int unicodeLength = TextStringToUCS4(rawString, &u);
-
-		for (int i = 0; i < unicodeLength; i++) {
-			int n = u_map->mapUnicode(u[i], buf, sizeof(buf));
-			for (int j = 0; j < n; j++) {
-				markup_text += buf[j];
-			}
-		}
-		gfree(u);
-	} else {
-		auto str = rawString->getCString();
-		markup_text += str;
-	}
-
-	return markup_text;
-}
+#ifdef NOPE
 
 static double resolution = 72.0;
 
-std::list<rect_t> getRectanglesForAnnot(Annot *annot) {
-	std::list<rect_t> rects;
-	const Ref ref = annot->getRef();
-	Object annotObj;
-	Object obj1;
-	PDFRectangle *rect = new PDFRectangle();
-
-	annot->getDoc()->getXRef()->fetch(ref.num, ref.gen, &annotObj);
-	annotObj.dictLookup("QuadPoints", &obj1);
-	AnnotQuadrilaterals *quads = new AnnotQuadrilaterals(obj1.getArray(), rect);
-
-	for (int i = 0; i < quads->getQuadrilateralsLength(); i++) {
-		rects.push_back({ quads->getX1(i), quads->getY1(i), quads->getX2(i), quads->getY3(i) });
-	}
-
-	delete quads;
-	delete rect;
-	annotObj.free();
-	obj1.free();
-
-	return rects;
-}
-
-void writeStampToFile(Annot *annot, const char* objectID, std::string imgDirectory) {
-	Gfx *gfx;
-	ImageOutputDev *imageOut;
-	PDFDoc *doc;
-	Page *page;
-	int pageNum;
-
-	std::string imgRoot = imgDirectory + "/";
-	imgRoot.append(objectID);
-
-	char *pf = new char[imgRoot.length() + 1];
-	std::strcpy(pf, imgRoot.c_str());
-
-	imageOut = new ImageOutputDev(pf, NULL, gFalse);
-	imageOut->enablePNG(gTrue);
+std::string getStampPNG(Annot *annot) {
+	PDFRectangle *origCropBox;
+	PDFRectangle *origMediaBox;
+	PDFRectangle *annotRect;
+	PDFRectangle *tmpAnnotRect;
+	double x1, x2, y1, y2;
 
 	doc = annot->getDoc();
 	pageNum = annot->getPageNum();
-	page = doc->getPage(pageNum);
+	page = doc->getPage(annot->getPageNum());
 
-	gfx = page->createGfx(imageOut, resolution, resolution, 0,
+	origCropBox = page->getCropBox();
+	origMediaBox = page->getMediaBox();
+
+
+	annot->getRect(&x1, &y1, &x2, &y2);
+	double annotWidth = x2 - x1;
+	double annotHeight = y2 - y1;
+	double edge = page->getCropWidth();
+
+	annot->setRect(edge, y1, edge + annotWidth, y2);
+	tmpAnnotRect = annot->getRect();
+
+	doc->replacePageDict(pageNum, 0, tmpAnnotRect, tmpAnnotRect);
+	doc->displayPage
+
+	gfx = page->createGfx(splashOut, resolution, resolution, 0,
 			gTrue, gFalse,
 			-1, -1, -1, -1,
 			gFalse, NULL, NULL);
 
+	// gfx->drawAnnot
+
 	annot->draw(gfx, gFalse);
 
-	delete pf;
-	delete imageOut;
+	return out;
 }
 
 
-std::string getTextForMarkupAnnot(UnicodeMap *u_map, Annot *annot) {
-	TextOutputDev *textOut;
-	TextPage *textPage;
-	PDFDoc *doc;
-	PDFRectangle *mediaBox;
-	Page *page;
-	int pageNum;
-	std::string text = "";
-
-	doc = annot->getDoc();
-	pageNum = annot->getPageNum();
-	page = doc->getPage(pageNum);
-
-	mediaBox = page->getMediaBox();
-
-	// Set up the text to be rendered
-	textOut = new TextOutputDev(NULL, gFalse, 0, gFalse, gFalse);
-
-	doc->displayPage(textOut, pageNum, resolution, resolution, 0, gTrue, gFalse, gFalse);
-	textPage = textOut->takeText();
-
-	auto rects = getRectanglesForAnnot(annot);;
-	for (auto rect : rects) {
-		GooString *str = textPage->getText(rect.xMin, mediaBox->y2 - rect.yMin, rect.xMax, mediaBox->y2 - rect.yMax);
-		if (text.length() > 0) {
-			text += " ";
-		}
-		text += gooStringToStdString(NULL, str);
-		delete str;
-	}
-
-
-	textPage->decRefCnt();
-	delete textOut;
-	return text;
-}
+#endif
 
 std::string annotationToJSON(annotation_t *annot) {
+	using json = nlohmann::json;
+
 	json out;
 
 	out["page"] = annot->page;
-	out["object_id"] = annot->object_id;
+	// out["object_id"] = annot->object_id;
 	out["motivation"] = annot->motivation;
 
 	if (annot->body_text.length() > 0) {
@@ -180,11 +91,105 @@ std::string annotationToJSON(annotation_t *annot) {
 		out["stamp_label"] = annot->stamp_label;
 	}
 
+	if (annot->body_png.length() > 0) {
+		out["stamp_body"] = annot->body_png;
+	}
+
 	return out.dump();
 }
 
+std::list<annotation_t> process_page(PopplerDocument *doc, int page_number) {
+	std::list<annotation_t> processed_annots;
 
-std::list<annotation_t> process_page(UnicodeMap *u_map, PDFDoc* doc, int page_number, std::string imgDirectory) {
+	PopplerPage *p = poppler_document_get_page(doc, page_number);
+	GList *l, *annots;
+
+	annots = poppler_page_get_annot_mapping(p);
+
+	for (l = annots; l; l = g_list_next(l)) {
+		PopplerAnnotMapping *m;
+		m = (PopplerAnnotMapping *) l->data;
+
+		switch(poppler_annot_get_annot_type(m->annot)) {
+		case PopplerAnnotType::POPPLER_ANNOT_TEXT: {
+			gchar *content;
+
+			content = poppler_annot_get_contents(m->annot);
+
+			annotation_t a = {
+					page_number,
+					"commenting",
+					NULL,
+					content,
+					"",
+					""
+			};
+
+			processed_annots.push_back(a);
+
+			g_free(content);
+			break;
+		}
+
+		case PopplerAnnotType::POPPLER_ANNOT_HIGHLIGHT:
+		case PopplerAnnotType::POPPLER_ANNOT_UNDERLINE: {
+			PopplerRectangle cropBox;
+			PopplerQuadrilateral quad;
+			std::string s;
+			auto markup = POPPLER_ANNOT_TEXT_MARKUP(m->annot);
+			auto quads = poppler_annot_text_markup_get_quadrilaterals(markup);
+
+			poppler_page_get_crop_box(p, &cropBox);
+
+			for (gint i = 0; i < quads->len; i++) {
+				PopplerRectangle rect;
+				quad = g_array_index(quads, PopplerQuadrilateral, i);
+
+				double xMin, yMin, xMax, yMax;
+
+				xMin = quad.p1.x;
+				yMin = quad.p1.y;
+				xMax = quad.p2.x;
+				yMax = quad.p3.y;
+
+				rect.x1 = xMin;
+				rect.y1 = (cropBox.y1 + cropBox.y2) - yMin;
+				rect.x2 = xMax;
+				rect.y2 = (cropBox.y1 + cropBox.y2) - yMax;
+
+				// auto b = poppler_page_get_selected_text(p, POPPLER_SELECTION_WORD, &rect);
+				auto b = poppler_page_get_text_for_area(p, &rect);
+
+				if (s.length()) {
+					s += "\n";
+				}
+
+				s += b;
+			}
+
+			annotation_t a = {
+					page_number,
+					"highlighting",
+					NULL,
+					"",
+					s,
+					""
+			};
+
+			processed_annots.push_back(a);
+			g_array_free(quads, TRUE);
+			break;
+		}
+		default:
+			break;
+		}
+	}
+
+	poppler_page_free_annot_mapping(annots);
+
+	return processed_annots;
+
+	/*
 	Page *page = doc->getPage(page_number);
 	Annots *annots = page->getAnnots();
 	std::list<annotation_t> processed_annots;
@@ -194,29 +199,20 @@ std::list<annotation_t> process_page(UnicodeMap *u_map, PDFDoc* doc, int page_nu
 		annot = annots->getAnnot(j);
 
 		switch (annot->getType()) {
-		case Annot::typeText: {
-			std::string body_text = gooStringToStdString(u_map, annot->getContents());
-
-			annotation_t a = {
-				page_number, annot->getId(), "commenting",
-				NULL,
-				body_text, ""
-			};
-
-			processed_annots.push_back(a);
-			break;
-		}
 
 		case Annot::typeStamp: {
 			AnnotStamp *stamp = static_cast<AnnotStamp *>(annot);
 
+			std::string pngRaw = getStampPNG(annot);
+			std::string pngEncoded;
+			Base64::Encode(pngRaw, &pngEncoded);
+
+			printf("\n\n%s\n\n", pngEncoded.c_str());
 
 			annotation_t a = {
 				page_number, annot->getId(), "bookmarking",
-				stamp->getSubject()->getCString(), "", ""
+				stamp->getSubject()->getCString(), "", "", pngEncoded
 			};
-
-			writeStampToFile(annot, std::to_string(stamp->getId()).c_str(), imgDirectory);
 
 			processed_annots.push_back(a);
 			break;
@@ -228,7 +224,7 @@ std::list<annotation_t> process_page(UnicodeMap *u_map, PDFDoc* doc, int page_nu
 
 			annotation_t a = {
 					page_number, annot->getId(), "highlighting",
-					NULL, "", getTextForMarkupAnnot(u_map, annot)
+					NULL, "", getTextForMarkupAnnot(u_map, annot), ""
 			};
 			processed_annots.push_back(a);
 			break;
@@ -241,6 +237,7 @@ std::list<annotation_t> process_page(UnicodeMap *u_map, PDFDoc* doc, int page_nu
 	}
 
 	return processed_annots;
+	*/
 }
 
 class AnnotationWorker : public StreamingWorker {
@@ -250,13 +247,9 @@ class AnnotationWorker : public StreamingWorker {
 
 		// TODO: handle cases where options weren't passed
 		v8::Local<v8::Value> pdfFilename_ = options->Get(Nan::New<v8::String>("pdfFilename").ToLocalChecked());
-		v8::Local<v8::Value> imageDirectory_ = options->Get(Nan::New<v8::String>("imageDirectory").ToLocalChecked());
 
 		v8::String::Utf8Value s(pdfFilename_);
 		pdfFilename = *s;
-
-		v8::String::Utf8Value t(imageDirectory_);
-		imageDirectory = *t;
 
 		if (!globalParams) {
 			globalParams = new GlobalParams();
@@ -267,23 +260,23 @@ class AnnotationWorker : public StreamingWorker {
 	}
 
 	void Execute (const AsyncProgressWorker::ExecutionProgress &progress) {
-		PDFDoc *doc;
-		UnicodeMap *uMap;
-		GooString *filename;
+		PopplerDocument *doc;
+		std::string fileURI = "file://" + pdfFilename;
+		GError *err = NULL;
+		int n;
 
-		uMap = globalParams->getTextEncoding();
+		doc = poppler_document_new_from_file(fileURI.c_str(), NULL, &err);
 
-		filename = new GooString(pdfFilename.c_str());
-		doc = PDFDocFactory().createPDFDoc(*filename, NULL, NULL);
-
-		if (!doc->isOk()) {
-			Nan::ThrowError("Could not open PDF at given filename.");
+		if (!doc) {
+			// FIXME: err->msg;
+			Nan::ThrowError("Error opening file");
 		}
 
-		for (int i = 1; i <= doc->getNumPages(); i++) {
-			if (this->closed()) break;
+		n = poppler_document_get_n_pages(doc);
 
-			std::list<annotation_t> page_annots = process_page(uMap, doc, i, imageDirectory);
+		for (int i = 0; i < n; i++) {
+			if (this->closed()) break;
+			std::list<annotation_t> page_annots = process_page(doc, i);
 
 			for (annotation_t annot : page_annots) {
 				if (this->closed()) break;
@@ -294,18 +287,10 @@ class AnnotationWorker : public StreamingWorker {
 		}
 
 		close();
-
-		delete filename;
-		delete doc;
-		uMap->decRefCnt();
-		// FIXME: I don't know how to deal with globalParams in a multithreaded environment
-		// delete globalParams;
 	}
 
 	private:
 	std::string pdfFilename;
-	std::string imageDirectory;
-
 };
 
 StreamingWorker * create_worker(Callback *data
