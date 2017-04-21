327251ffcb66f3a1fbcb4bf873dfb79e67bf5182
Patrick Golden <ptgolden0@gmail.com>
2017-04-21 15:59:56 -0400

    Encode PNG images as base64

--- cpp/oac.cc
+++ cpp/oac.cc
@@ -9,8 +9,10 @@
 #include <goo/PNGWriter.h>
 #include <cairo.h>
 
+#include "base64.h"
 #define JSON_NOEXCEPTION 1
 #include "json.hpp"
+
 
 using nlohmann::json;
 
@@ -33,6 +35,80 @@ struct OacAnnot {
 	string body_image;
 	string target_text;
 };
+
+uint32_t
+apply_alpha(uint32_t level, uint32_t alpha) {
+	return (level * 255 + alpha / 2) / alpha;
+}
+
+string
+raw_png_from_cairo_surface(cairo_surface_t *surface) {
+	string raw_png;
+	char *raw_png_c;
+	int width, height, stride;
+	unsigned char *data;
+	FILE *fp;
+	long fsize;
+	ImgWriter *writer;
+
+	width = cairo_image_surface_get_width(surface);
+	height = cairo_image_surface_get_height(surface);
+	stride = cairo_image_surface_get_stride(surface);
+	cairo_surface_flush(surface);
+	data = cairo_image_surface_get_data(surface);
+
+
+	writer = new PNGWriter(PNGWriter::RGBA);
+	fp = tmpfile();
+	if (!writer->init(fp, width, height, 72.0, 72.0)) {
+		fprintf(stderr, "Error writing file");
+		exit(-1);
+	}
+
+	unsigned char *row = (unsigned char *)gmallocn(width, 4);
+	for (int y = 0; y < height; y++) {
+		uint32_t *pixel = (uint32_t *)(data + y * stride);
+		unsigned char *rowp = row;
+		for (int x = 0; x < width; x++, pixel++) {
+			uint8_t r, g, b, a;
+
+			a = ((*pixel & 0xff000000) >> 24);
+
+			if (a == 0) {
+				r = g = b = 0;
+			} else {
+				r = apply_alpha(((*pixel & 0xff0000) >> 16), a);
+				g = apply_alpha(((*pixel & 0x00ff00) >> 8), a);
+				b = apply_alpha(((*pixel & 0x0000ff) >> 0), a);
+			}
+
+			*rowp++ = r;
+			*rowp++ = g;
+			*rowp++ = b;
+			*rowp++ = a;
+		}
+
+		writer->writeRow(&row);
+	}
+	gfree(row);
+	writer->close();
+	fflush(fp);
+	fseek(fp, 0, SEEK_END);
+	fsize = ftell(fp);
+	std::rewind(fp);
+	raw_png_c = (char *)malloc(fsize + 1);
+
+	if (fread(raw_png_c, fsize, 1, fp) != 1) {
+		fprintf(stderr, "Error reading stamp PNG to buffer\n");
+		exit(-1);
+	}
+	fclose(fp);
+
+	raw_png.assign(raw_png_c, fsize);
+	free(raw_png_c);
+
+	return raw_png;
+}
 
 string
 annotation_action_get_motivation(AnnotationAction action) {
@@ -96,6 +172,8 @@ oac_annot_set_body_text(OacAnnot *oac_annot, PopplerAnnot *annot) {
 
 void
 oac_annot_set_body_image(OacAnnot *oac_annot, PopplerAnnot *annot, PopplerPage *page) {
+	string png_base64;
+	string png_raw;
 	cairo_t *cairo;
 	cairo_surface_t *surface;
 
@@ -110,16 +188,14 @@ oac_annot_set_body_image(OacAnnot *oac_annot, PopplerAnnot *annot, PopplerPage *
 	poppler_page_render_annot(page, annot, cairo);
 
 	cairo_destroy(cairo);
-
 	cairo_surface_flush(surface);
 
-	auto status = cairo_surface_write_to_png(surface, "/tmp/png/test.png");
-	printf("%s\n", cairo_status_to_string(status));
+	png_raw = raw_png_from_cairo_surface(surface);
+	Base64::Encode(png_raw, &png_base64);
+	oac_annot->body_image = png_base64;
 
 	cairo_surface_finish(surface);
 	cairo_surface_destroy(surface);
-
-	return;
 }
 
 void
