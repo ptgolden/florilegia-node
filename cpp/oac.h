62667f2f23d452a356c1821466405d92c4a88c51
Patrick Golden <ptgolden0@gmail.com>
2017-04-22 13:53:03 -0400

    Only include needed functions in oac.h

--- cpp/oac.h
+++ cpp/oac.h
@@ -22,18 +22,6 @@ struct OacAnnot {
 	string target_text;
 };
 
-string annotation_action_get_motivation(AnnotationAction action);
-
-void oac_annot_set_annotation_action(OacAnnot *oac_annot, PopplerAnnot *annot);
-
-void oac_annot_set_body_text(OacAnnot *oac_annot, PopplerAnnot *annot);
-
-void oac_annot_set_body_image(OacAnnot *oac_annot, PopplerAnnot *annot);
-
-void oac_annot_set_target_text(OacAnnot *oac_annot, PopplerAnnot *annot);
-
 string oac_annot_to_json(OacAnnot *oac_annot);
-
-void oac_annot_from_mapping(OacAnnot *oac_annot, PopplerAnnotMapping *m, PopplerPage *p);
 
 list<OacAnnot> poppler_document_get_oac_annots(PopplerDocument *doc, int page_number);
